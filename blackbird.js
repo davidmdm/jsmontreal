f6b4f2fe814345ef53e6951ef98e16fcf5508bb6
davidmdm <david.desmarais-michaud@pj.ca>
2018-09-02 16:15:30 -0400

    error handling

--- blackbird.js
+++ blackbird.js
@@ -4,30 +4,92 @@ class Blackbird {
   constructor(fn) {
     this.state = 'pending';
     this.callbacks = [];
+    this.handlers = [];
+
+    const reject = err =>
+      process.nextTick(() => {
+        if (err instanceof Blackbird) {
+          return err.then(reject, reject);
+        }
+        this.error = err;
+        this.state = 'rejected';
+        if (this.handlers.length === 0) {
+          console.error('unhandled promise rejection error %s: %s', err.message, err.stack);
+        } else {
+          this.handlers.forEach(handler => handler(this.error));
+        }
+      });
 
     const resolve = value =>
       process.nextTick(() => {
         if (value instanceof Blackbird) {
-          return value.then(resolve);
+          return value.then(resolve, reject);
         }
         this.value = value;
         this.state = 'resolved';
         this.callbacks.forEach(cb => cb(this.value));
       });
 
-    fn(resolve);
+    fn(resolve, reject);
   }
 
-  then(cb) {
+  then(cb, handler) {
     if (this.state === 'resolved') {
-      return Blackbird.resolve(cb(this.value));
+      return new Blackbird((resolve, reject) => {
+        try {
+          resolve(cb(this.value));
+        } catch (err) {
+          reject(err);
+        }
+      });
     }
 
-    return new Blackbird(resolve => this.callbacks.push(x => resolve(cb(x))));
+    if (this.state === 'rejected') {
+      return new Blackbird((resolve, reject) => {
+        if (!handler) {
+          return reject(err);
+        }
+        try {
+          resolve(handler(this.error));
+        } catch (err) {
+          reject(err);
+        }
+      });
+    }
+
+    return new Blackbird((resolve, reject) => {
+      this.callbacks.push(x => {
+        try {
+          resolve(cb(x));
+        } catch (err) {
+          reject(err);
+        }
+      });
+
+      if (handler) {
+        this.handlers.push(x => {
+          try {
+            resolve(handler(x));
+          } catch (err) {
+            reject(err);
+          }
+        });
+      } else {
+        this.handlers.push(x => reject(x));
+      }
+    });
+  }
+
+  catch(handler) {
+    return this.then(x => x, handler);
   }
 
   static resolve(value) {
     return new Blackbird(resolve => resolve(value));
+  }
+
+  static reject(err) {
+    return new Blackbird((_, reject) => reject(err));
   }
 
   static all(birds) {
@@ -47,6 +109,21 @@ class Blackbird {
   }
 }
 
-const asyncReturn = (val, ms) => new Blackbird(resolve => setTimeout(resolve, ms, val));
+const fs = require('fs');
 
-Blackbird.all([asyncReturn('hello', 1000), asyncReturn('jsmontreal', 2000)]).then(results => console.log(...results));
+const readFile = file =>
+  new Blackbird((resolve, reject) =>
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  );
+
+readFile('./.gitignore')
+  .then(console.log)
+  .then(() => readFile('./doesnotexist'))
+  .then(() => console.log('weird there should be no file'))
+  .catch(err => console.error('error:', err));
