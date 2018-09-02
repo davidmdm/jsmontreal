fb88aebc9dff12fa54d2b4413296581177d07103
davidmdm <david.desmarais-michaud@pj.ca>
2018-09-02 18:10:16 -0400

    Async implemented

--- async.js
+++ async.js
@@ -18,18 +18,38 @@ const multiply = async function(a, b) {
 
 multiply(5, 6).then(console.log);
 
-// therefore we should be able to write a function "Async"
-
 const Async = function(generator) {
-  return function() {
-    return; // promise
+  const _runner = (it, val, cb) => {
+    const { value, done } = it.next(val);
+
+    if (done) {
+      return cb(null, value);
+    }
+
+    Promise.resolve(value)
+      .then(res => _runner(it, res, cb))
+      .catch(cb);
+  };
+
+  return function(...args) {
+    const it = generator(...args);
+
+    return new Promise((resolve, reject) => {
+      _runner(it, undefined, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
   };
 };
-
-// such that this function is analagous to async/await
 
 const add = Async(function*(a, b) {
   const x = yield a;
   const y = yield b;
   return x + y;
 });
+
+add(6, 5).then(console.log);
