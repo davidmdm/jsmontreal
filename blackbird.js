cd816d129ffbe155709941cb58c6265371e0bab7
davidmdm <david.desmarais-michaud@pj.ca>
2018-09-02 10:58:04 -0400

    added then

--- blackbird.js
+++ blackbird.js
@@ -3,12 +3,24 @@
 class Blackbird {
   constructor(fn) {
     this.state = 'pending';
+    this.callbacks = [];
 
     const resolve = value => {
       this.value = value;
       this.state = 'resolved';
+      this.callbacks.forEach(cb => cb(this.value));
     };
 
     fn(resolve);
   }
+
+  then(cb) {
+    if (this.state === 'resolved') {
+      return new Blackbird(resolve => resolve(cb(this.value)));
+    }
+
+    return new Blackbird(resolve => this.callbacks.push(x => resolve(cb(x))));
+  }
 }
+
+new Blackbird(resolve => setTimeout(resolve, 1000, 'hello Jsmontreal')).then(console.log);
